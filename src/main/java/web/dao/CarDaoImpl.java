0867e98813ba7eabb5eb15c5aef78286556990c5
Denis Hairullin <denis.vic82@gmail.com>
2022-06-27 00:41:39 +0500

    Car list moved to constant field

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -2,23 +2,28 @@ package web.dao;
 
 import org.springframework.stereotype.Repository;
 import web.model.Car;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class CarDaoImpl implements CarDao {
+    private static final List<Car> CARS = List.of(
+            new Car("Audi", "A7", "Black"),
+            new Car("Jaguar", "XJ", "Dark blue"),
+            new Car("Tesla", "Model X", "White"),
+            new Car("Nessan", "X-Trail", "Orange"),
+            new Car("Volkswagen", "Passat", "Gray")
+    );
+
     @Override
     public List<Car> listCars() {
-        return List.of(
-                new Car("Audi", "A7", "Black"),
-                new Car("Jaguar", "XJ", "Dark blue"),
-                new Car("Tesla", "Model X", "White"),
-                new Car("Nessan", "X-Trail", "Orange"),
-                new Car("Volkswagen", "Passat", "Gray")
-        );
+        return CARS;
     }
 
     @Override
     public List<Car> listCars(int count) {
-        return listCars().stream().limit(count).toList();
+        return count >= 0 ? listCars().stream().limit(count).collect(Collectors.toList())
+                : Collections.emptyList();
     }
 }
